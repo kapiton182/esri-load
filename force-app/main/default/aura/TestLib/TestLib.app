f4f101e224f6b7fb737e603a4dba9ebf1649a7f3
Dima <kapiton182@gmail.com>
2019-12-25 23:22:51 +0300

    init

---
+++ force-app/main/default/aura/TestLib/TestLib.app
@@ -0,0 +1,3 @@
+<aura:application>
+<c:testSetUpLib />
+</aura:application>	
---
+++ force-app/main/default/lwc/testSetUpLib/testSetUpLib.js
@@ -0,0 +1,72 @@
+/* eslint-disable no-undef */
+/* eslint-disable @lwc/lwc/no-async-operation */
+/* eslint-disable no-console */
+import { LightningElement, track } from "lwc";
+
+export default class TestSetUpLib extends LightningElement {
+  mapl;
+
+  renderedCallback() {
+    setTimeout(() => {
+      try {
+        let srcElem = this.template.querySelector(".src");
+        let script = document.createElement("script");
+        script.src = "https://unpkg.com/leaflet@1.5.1/dist/leaflet.js";
+
+        srcElem.appendChild(script);
+
+        let link = document.createElement("link"); 
+        link.href = "https://unpkg.com/leaflet@1.5.1/dist/leaflet.css";
+        link.rel = "stylesheet";
+
+        srcElem.appendChild(link);
+      } catch (e) {
+        console.log(e);
+      }
+      setTimeout(() => {
+        let srcElem = this.template.querySelector(".src");
+
+        let script2 = document.createElement("script");
+        script2.src =
+          "https://unpkg.com/esri-leaflet@2.3.2/dist/esri-leaflet.js";
+
+        srcElem.appendChild(script2);
+        let mapElem = this.template.querySelector(".map");
+            this.mapl = L.map(mapElem).setView([40.91, -96.63], 4);
+
+            L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+              attribution:
+                '&copy; <a href="https://osm.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(this.mapl);
+        setTimeout(() => {
+          let srcElem = this.template.querySelector(".src");
+
+          let script3 = document.createElement("script");
+          script3.src =
+            "https://unpkg.com/esri-leaflet-geocoder@2.3.2/dist/esri-leaflet-geocoder.js";
+          let link2 = document.createElement("link");
+          link2.href =
+            "https://unpkg.com/esri-leaflet-geocoder@2.3.2/dist/esri-leaflet-geocoder.css";
+          link2.rel = "stylesheet";
+          srcElem.appendChild(link2);
+
+          srcElem.appendChild(script3);
+          setTimeout(() => {
+            
+
+            let searchControl = L.esri.Geocoding.geosearch().addTo(this.mapl);
+
+            let results = L.layerGroup().addTo(this.mapl);
+
+            searchControl.on("results", function(data) {
+              results.clearLayers();
+              for (let i = data.results.length - 1; i >= 0; i--) {
+                results.addLayer(L.marker(data.results[i].latlng));
+              }
+            });
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }
+}
